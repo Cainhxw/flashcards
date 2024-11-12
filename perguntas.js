478e14d4845a95685c8e85b8efd7cc92330ea526
Caio Lopes <00001092743418sp@al.educacao.sp.gov.br>
2024-11-12 15:37:41 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Quem é Zé Bombom?',
+    'O diretor da escola Moraes Salles'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem é a professora de geografia?',
+    'Claudinha'
+)
+
+criaCartao(
+    'Programação',
+    'Quem é o professor de tecnologia?',
+    'O careca do Márcio'
+)
+
+criaCartao(
+    'Aulas',
+    'Qual a melhor aula pra dormir?',
+    'As aulas da Maria Cecília'
+)
